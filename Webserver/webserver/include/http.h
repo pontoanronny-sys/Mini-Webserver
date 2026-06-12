48fbd2a4d59eba2d6add720b82d547c9cff37ea8
pontoanronny-sys <pontoan.ronny@gmail.com>
2026-06-12 16:44:52 +0800

    Delete Webserver/webserver directory

--- Webserver/webserver/include/http.h
+++
@@ -1,8 +0,0 @@
-#ifndef HTTP_H
-#define HTTP_H
-
-void handle_http_request(int client_socket, const char *web_root);
-const char *get_mime_type(const char *path);
-void send_http_error(int client_socket, int status_code, const char *status_text);
-
-#endif
